1165c81002df88b9f6053891c91e8fc10c211ee0
Mustafa Abdelrahman <mustafausama@outlook.com>
2020-08-15 01:56:24 +0200

    First touches

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// Express app
+const app = express();
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Database
+const db = require("./config/keys").mongoDB;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Passport
+
+// Routes
+
+// App listen
+const port = process.env.PORT || 5007;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
